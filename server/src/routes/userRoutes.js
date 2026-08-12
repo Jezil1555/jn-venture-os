b3852d07f31ee7f08e9fda1b547cd70c06a0d5b2
Jezil1555 <jezilsiddique555@gmail.com>
2026-08-12 14:12:33 +0300

    Update userRoutes.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUsers, patchUserStatus } from '../controllers/userController.js';
+import { getUsers, patchUserStatus, removeUser } from '../controllers/userController.js';
 import { requireAuth, requireRole } from '../middleware/auth.js';
 import { asyncHandler } from '../middleware/errorHandler.js';
 
@@ -8,5 +8,6 @@ const router = Router();
 router.use(requireAuth, requireRole('admin'));
 router.get('/', asyncHandler(getUsers));
 router.patch('/:id/status', asyncHandler(patchUserStatus));
+router.delete('/:id', asyncHandler(removeUser));
 
 export default router;
